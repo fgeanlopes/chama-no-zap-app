8e5b1b909ac6751992266751cdd2418a4b0b975c
gean <gean@pop-os.localdomain>
2020-06-11 20:02:39 -0300

    update

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [numero, setNumero] = useState("");
+
+  function convertNumero() {}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* LOGO DA APLICACAO */}
+      <Text>Chama Zap</Text>
+
+      <Text>Adicione o número no campo abaixoque deseja chamar.</Text>
+
+      <TextInput
+        style={styles.input_entrada}
+        value={numero}
+        onChangeText={(numero) => setNumero(numero)}
+        placeholder={"Adicione o número aqui."}
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={styles.btn_default} onPress={convertNumero}>
+        <Text>Chamar no Zap</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
